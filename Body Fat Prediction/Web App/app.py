c0a6bf993c3607e503f41c93702a5fe97b964493
imksprateek <ksprateek2004@gmail.com>
2024-06-17 10:56:44 +0530

    Enhanced readability

--- Body Fat Prediction/Web App/app.py
+++ Body Fat Prediction/Web App/app.py
@@ -33,7 +33,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Predicted body fat: {}'.format(output))
+    return render_template('index.html', prediction_text='Predicted body fat: {}%'.format(output))
 
 
 if __name__ == "__main__":

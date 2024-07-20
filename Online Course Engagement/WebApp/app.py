7f6320c878e89655a637c02f8b7049810af6531e
Siddhant Tiwari <stiwari.freelance@gmail.com>
2024-07-20 19:29:17 +0530

    Fixed model path

--- Online Course Engagement/WebApp/app.py
+++ Online Course Engagement/WebApp/app.py
@@ -1,7 +1,7 @@
 import joblib
 import streamlit as st
 
-MODEL_PATH = "../Model/saved_models/02_adaboost_Extended.joblib"
+MODEL_PATH = "Model/saved_models/02_adaboost_Extended.joblib"
 loaded_model = joblib.load(MODEL_PATH)
 
 
